4d6a77b6f4561f17dde5c9c1d33c43d74411fe61
Brandon Espinoza <espinozab100@gmail.com>
2016-02-28 19:43:50 -0600

    Made an Admin Model with functionality

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,5 +2,5 @@ class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 end
